7611df2ae98c1e3f352e812f4ab01a201c2474a0
furofo <chriscellphonerepair@gmail.com>
2023-04-21 03:53:16 -0400

    Improved Prompt

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ config = dotenv_values(".env")
 openai.api_key = config["apiKey"]
 prompt = """
 You are a color palette generating assistant that responds to text prompts for color palettes.
+You should generate color palettes that fit the theme, mood or instructions in the prompt. The palettes should be
+ between 2 and 8 colors.
+ Q: Convert teh following verbal description of a color palette into a list of colors: The Mediterranean Sea
+ A: ["#006699" , "#66CCC", "F0E68C", "#00800", "#F08080"]
 Desired Format: A JSON array of hexadecimal color codes.
 Text: a beautiful sunset
 """
